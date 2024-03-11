299ba6a9f39df7646b214f8645847468b0c40e5b
LanzeG <bermas.lester10@gmail.com>
2024-03-11 12:52:43 +0800

    commit ni lester

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import os
 import pandas as pd
 from fuzzywuzzy import fuzz
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import random
 from datetime import datetime
 import json
 import re
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill, Font
+from flask_socketio import SocketIO
+from time import sleep
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 def get_template_header(file_path):
     return pd.read_excel(file_path).columns.to_list()
@@ -28,7 +31,7 @@ def find_header(row_values, template_header):
 
 def get_index_of_header(excel_file_path, template_header) -> tuple[int, list]:
     work_book = pd.read_excel(excel_file_path, sheet_name=None, header=None)
-    for sheet_name, sheet_data in work_book.items():
+    for _, sheet_data in work_book.items():
         if sheet_data.empty:
             return 0, []
         for index, row in sheet_data.iterrows():
@@ -114,7 +117,6 @@ def extract_address(output_file_path, address_column_name, output_address_file_p
     address_data = output_data[address_column_name].dropna()
     address_data.to_excel(output_address_file_path, index=False)
 
-
 def auto_fit_columns(excel_file_path):
     wb = load_workbook(excel_file_path)
     for sheet_name in wb.sheetnames:
@@ -136,86 +138,98 @@ def auto_fit_columns(excel_file_path):
     wb.save(excel_file_path)
 
 
+@app.route('/merge', methods=['POST'])
+def merge():
+    directory_path = "C:\\Users\\SPM\\Desktop\\ONLY SAVE FILE HERE\\SIT-OJT\\Requests"
 
+    bank_name = request.form['bank_name']
+    folder_path = os.path.join(directory_path, bank_name)
+    files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    template_header = get_template_header("Template.xlsx")
+    datas = [template_header]
 
+    mapping = {
+        "REQUEST DATE": "DATE REQUESTED",
+        "REQUEST NAME": "REQUESTED BY"
+        # Add more mappings as needed
+    }
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    message = None
-    bank_names = []
-    for dirpath, dirnames, filenames in os.walk("C:\\Users\\SPM\Desktop\\ONLY SAVE FILE HERE\\SIT-OJT\\Requests"):
-        bank_names.extend(dirnames)
-        break  # Only the top-level directories are needed
+    existing_headers = set()
 
-    if request.method == 'POST':
-        bank_name = request.form['bank_name']
-        folder_path = os.path.join("C:\\Users\\SPM\Desktop\\ONLY SAVE FILE HERE\\SIT-OJT\\Requests", bank_name)
-        files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-        template_header = get_template_header("Template.xlsx")
-        datas = [template_header]
+    total_files = len(files)
 
-        mapping = {
-            "REQUEST DATE": "DATE REQUESTED",
-            "REQUEST NAME": "REQUESTED BY"
-            # Add more mappings as needed
-        }
+    for i, file in enumerate(files):
+        excel_file_path = os.path.join(folder_path, file)
+        index_header, current_header = get_index_of_header(excel_file_path, template_header)
+        work_book = pd.read_excel(excel_file_path, sheet_name=None, header=index_header)
 
-        existing_headers = set()
-
-        for file in files:
-            excel_file_path = os.path.join(folder_path, file)
-            index_header, current_header = get_index_of_header(excel_file_path, template_header)
-            work_book = pd.read_excel(excel_file_path, sheet_name=None, header=index_header)
-            for sheet_name, sheet_data in work_book.items():
-                for index, row in sheet_data.iterrows():
-                    output_row = []
-                    for header in template_header:
-                        value = None
-                        for col_header, col_value in row.items():
-                            if compare_string(header, col_header):
-                                value = col_value
-                                break
-                        output_row.append(value)
-                    datas.append(output_row)
+        for _, sheet_data in work_book.items():
+            for _, row in sheet_data.iterrows():
+                output_row = []
+                for header in template_header:
+                    value = None
+                    for col_header, col_value in row.items():
+                        if compare_string(header, col_header):
+                            value = col_value
+                            break
+                    output_row.append(value)
+                datas.append(output_row)
 
                    # Append missing columns from the file
-                    for col_header in row.keys():
-                        mapped_header = map_header(col_header, mapping)
-                        # if mapped_header.lower() not in [h.lower() for h in template_header] and mapped_header not in existing_headers and compare_string(mapped_header, col_header):
-                        #     datas[0].append(mapped_header)
-                        #     output_row.append(row[col_header])
-                        #     existing_headers.add(mapped_header)
-                        if mapped_header.lower() not in [h.lower() for h in template_header] and mapped_header not in existing_headers and not (col_header.strip() == "" or col_header.startswith("Unnamed")) and compare_string(mapped_header, col_header):
-                            datas[0].append(mapped_header)
-                            output_row.append(row[col_header])
-                            existing_headers.add(mapped_header)
+                for col_header in row.keys():
+                    mapped_header = map_header(col_header, mapping)
+                    # if mapped_header.lower() not in [h.lower() for h in template_header] and mapped_header not in existing_headers and compare_string(mapped_header, col_header):
+                    #     datas[0].append(mapped_header)
+                    #     output_row.append(row[col_header])
+                    #     existing_headers.add(mapped_header)
+                    if mapped_header.lower() not in [h.lower() for h in template_header] and mapped_header not in existing_headers and not (col_header.strip() == "" or col_header.startswith("Unnamed")) and compare_string(mapped_header, col_header):
+                        datas[0].append(mapped_header)
+                        output_row.append(row[col_header])
+                        existing_headers.add(mapped_header)
 
-        output_work_book = pd.DataFrame(datas[1:], columns=datas[0])
-        random_number = "".join([str(random.randint(0, 9)) for _ in range(4)])
-        current_date = datetime.now().strftime("%Y-%m-%d")
-        output_file_name = f"Output-{bank_name}-{current_date}-{random_number}.xlsx"
-        output_file_path = os.path.join("C:\\Users\\SPM\Desktop\\ONLY SAVE FILE HERE\\SIT-OJT\\Merged", output_file_name)
-        output_work_book.to_excel(output_file_path, index=False)
+            socketio.emit("update progress", (i + 1) / (total_files + 1) * 100)
 
-        campaign_file_path = 'campaign_list.json' 
-        fill_missing_values(output_file_path, campaign_file_path)
+    output_work_book = pd.DataFrame(datas[1:], columns=datas[0])
+    random_number = "".join([str(random.randint(0, 9)) for _ in range(4)])
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    output_file_name = f"Output-{bank_name}-{current_date}-{random_number}.xlsx"
+    output_file_path = os.path.join(directory_path, output_file_name)
+    output_work_book.to_excel(output_file_path, index=False)
 
-        address_column_name = "ADDRESS"  # Replace with the actual column name containing the address
-        output_address_file_name = f"Output-Address-{bank_name}-{current_date}-{random_number}.xlsx"
-        output_address_file_path = os.path.join("C:\\Users\\SPM\Desktop\\ONLY SAVE FILE HERE\\SIT-OJT\\Merged\\Area", output_address_file_name)
-        extract_address(output_file_path, address_column_name, output_address_file_path)
+    campaign_file_path = 'campaign_list.json' 
+    fill_missing_values(output_file_path, campaign_file_path)
 
-        message = f"Excel file created successfully for {bank_name}. Output file: <strong>{output_file_name}</strong>. Address file: <strong>{output_address_file_name}</strong>"
+    address_column_name = "ADDRESS"  # Replace with the actual column name containing the address
+    output_address_file_name = f"Output-Address-{bank_name}-{current_date}-{random_number}.xlsx"
+    output_address_file_path = os.path.join(directory_path, output_address_file_name)
+    extract_address(output_file_path, address_column_name, output_address_file_path)
+
+    message = f"Excel file created successfully for {bank_name}. Output file: <strong>{output_file_name}</strong>. Address file: <strong>{output_address_file_name}</strong>"
 
           # Call auto_fit_columns function after saving the Excel file
-        auto_fit_columns(output_file_path)
-        auto_fit_columns(output_address_file_path)
-        
+    auto_fit_columns(output_file_path)
+    auto_fit_columns(output_address_file_path)
 
-    return render_template('index.html', message=message, bank_names=bank_names)
+    socketio.emit("update progress", (total_files + 1) / (total_files + 1) * 100)
+
+    sleep(1)
+
+    data_to_return = {'message': message, 'status': 'success'}
+
+    return jsonify(data_to_return)
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+    directory_path = "C:\\Users\\SPM\\Desktop\\ONLY SAVE FILE HERE\\SIT-OJT\\Requests"
+
+    # Get bank names using folder
+    bank_names = [folder.upper() for folder in os.listdir(directory_path) if os.path.isdir(os.path.join(directory_path, folder))]
+        
+    return render_template('index.html', bank_names=bank_names)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    socketio.run(app=app, debug=True, host="0.0.0.0", port=25565)
 
 
