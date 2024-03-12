cc086483250e467f42c4be232124e8f8d3aea9ee
Soj <josellecallora08@gmail.com>
2024-03-13 07:39:32 +0800

    added comments on functions

--- functions.py
+++ functions.py
@@ -5,9 +5,11 @@ from openpyxl.styles import PatternFill, Font
 from openpyxl import load_workbook
 import pandas as pd
 
+# Function to get header from an Excel file
 def get_template_header(file_path):
     return pd.read_excel(file_path).columns.astype(str).str.upper().to_list()
 
+# Function to get the total number of rows in an Excel file
 def get_total_rows(file_path):
     xls = pd.ExcelFile(file_path)
     total_rows = 0
@@ -18,10 +20,12 @@ def get_total_rows(file_path):
 
     return total_rows
 
+# Function to compare two strings with a given threshold using fuzzy matching
 def compare_string(str1, str2, threshold=90):
     similarity_ratio = fuzz.token_set_ratio(str(str1).lower(), str(str2).lower())
     return similarity_ratio >= threshold
 
+# Function to find a header in a list of row values
 def find_header(row_values, template_header):
     row_list = row_values.tolist()
     for header in template_header:
@@ -30,6 +34,7 @@ def find_header(row_values, template_header):
                 return True
     return False
 
+# Function to get the index of the header in an Excel file
 def get_index_of_header(excel_file_path, template_header) -> int:
     work_book = pd.read_excel(excel_file_path, sheet_name=None, header=None)
     for _, sheet_data in work_book.items():
@@ -40,52 +45,14 @@ def get_index_of_header(excel_file_path, template_header) -> int:
                 return index
     return 0
 
+# Function to map a header based on a given mapping dictionary
 def map_header(header, mapping):
     for key, value in mapping.items():
         if compare_string(header.lower(), key.lower()):
             return value
     return header
-# #GETTING ONLY FIRST 6 CHAR IN CH CODE
-# # def fill_missing_values(excel_file_path, campaign_file_path):
-# #     with open(campaign_file_path, 'r') as file:
-# #         campaign_data = json.load(file)
-    
-# #     excel_data = pd.read_excel(excel_file_path)
-# #     for index, row in excel_data.iterrows():
-# #         if pd.isna(row['BANK']):
-# #             ch_code = str(row['CH CODE'])[:6]  # Get first 6 characters of CH CODE
-# #             if ch_code in campaign_data:
-# #                 excel_data.at[index, 'BANK'] = campaign_data[ch_code]['BANK']
-# #         if pd.isna(row['PLACEMENT']):
-# #             ch_code = str(row['CH CODE'])[:6]  # Get first 6 characters of CH CODE
-# #             if ch_code in campaign_data:
-# #                 excel_data.at[index, 'PLACEMENT'] = campaign_data[ch_code]['PLACEMENT']
-    
-# #     excel_data.to_excel(excel_file_path, index=False)
 
-
-# # GETTING ONLY FIRST NUMBER AND LETTERS IN CH CODE
-# def fill_missing_values(excel_file_path, campaign_file_path):
-#     with open(campaign_file_path, 'r') as file:
-#         campaign_data = json.load(file)
-    
-#     excel_data = pd.read_excel(excel_file_path)
-#     for index, row in excel_data.iterrows():
-#         if pd.isna(row['BANK']):
-#             ch_code_match = re.search(r'^(\d+[A-Z]+)', str(row['CH CODE']))  # Extract first numbers and letters from CH CODE
-#             if ch_code_match is not None:
-#                 ch_code = ch_code_match.group(1)
-#                 if ch_code in campaign_data:
-#                     excel_data.at[index, 'BANK'] = campaign_data[ch_code]['BANK']
-#         if pd.isna(row['PLACEMENT']):
-#             ch_code_match = re.search(r'^(\d+[A-Z]+)', str(row['CH CODE']))  # Extract first numbers and letters from CH CODE
-#             if ch_code_match is not None:
-#                 ch_code = ch_code_match.group(1)
-#                 if ch_code in campaign_data:
-#                     excel_data.at[index, 'PLACEMENT'] = campaign_data[ch_code]['PLACEMENT']
-    
-#     excel_data.to_excel(excel_file_path, index=False)
-
+# Function to highlight and fill missing values in specific columns
 def highlight_n_fill_missing_values(excel_file_path, campaign_file_path):
     with open(campaign_file_path, 'r') as file:
         campaign_data = json.load(file)
@@ -124,7 +91,7 @@ def highlight_n_fill_missing_values(excel_file_path, campaign_file_path):
     # Save the workbook
     book.save(excel_file_path)
 
-# GETTING ONLY FIRST NUMBER AND LETTERS IN CH CODE || EXCLUDE ALL STRING VALUE
+# Function to drop rows with CH CODE that has all-letter value
 def drop_row_with_one_cell(excel_file_path):
     excel_data = pd.read_excel(excel_file_path)
 
@@ -136,11 +103,13 @@ def drop_row_with_one_cell(excel_file_path):
     # Save the modified DataFrame to an Excel file
     excel_data.to_excel(excel_file_path, index=False)
 
+# Function to extract and save address data from an output Excel file
 def extract_address(output_file_path, address_column_name, output_address_file_path):
     output_data = pd.read_excel(output_file_path)
     address_data = output_data[address_column_name].dropna()
     address_data.to_excel(output_address_file_path, index=False)
 
+# Function to auto-fit columns in an Excel file
 def auto_fit_columns(excel_file_path):
     wb = load_workbook(excel_file_path)
     for sheet_name in wb.sheetnames:
@@ -163,4 +132,5 @@ def auto_fit_columns(excel_file_path):
             adjusted_width = (max_length + 2) * 1.2  # Adjusted width with a factor for padding
             ws.column_dimensions[column_cells[0].column_letter].width = adjusted_width
 
+    # Save the workbook
     wb.save(excel_file_path)
