b98ac07d07a04a6ccef5a1456c63ee7ec7d71c7b
soj <josellecallora08@gmail.com>
2024-05-10 15:59:36 +0800

    update

--- functions.py
+++ functions.py
@@ -15,6 +15,46 @@ def get_total_rows(file_path) -> int:
 def clean_string(input_string):
     return re.sub(r'[^a-zA-Z0-9\s]', ' ', input_string).upper()
 
+def clean_address(address):
+    """
+    Clean and normalize an address.
+
+    Parameters:
+    address (str): Input address string.
+
+    Returns:
+    str: Cleaned and normalized address string.
+    """
+    address = re.sub(r"[^a-zA-Z0-9\s]", " ", re.sub(r"[^\w\s`~!@#$%^&*()\_\-+=\[\]{};:'\"\\|,<.>\/]", "N", address.upper().replace('Ñ', 'N'))).split()
+
+    abbreviation = {
+        "GEN": "GENERAL",
+        "STA": "SANTA",
+        "STO": "SANTO",
+        "NATIONAL CAPITAL REGION": "NCR",
+        "METRO MANILA": "NCR"
+    }
+    
+    for key, value in abbreviation.items():
+        address = [value if word == key else word for word in address]
+
+    address = ' '.join(list(filter(lambda item: item.strip(), address)))
+
+    area = {
+        "NATIONAL CAPITAL REGION": "NCR",
+        "METRO MANILA": "NCR",
+        "AUTONOMOUS REGION IN MUSLIM MINDANAO": "ARMM",
+        "BANGSAMORO AUTONOMOUS REGION IN MUSLIM MINDANAO": "BARMM",
+        "CORDILLERA ADMINISTRATIVE REGION": "CAR",
+        "SOCSARGEN": "SOCCSKSARGEN",
+        "CITY": " CITY "        
+    }
+        
+    for key, value in area.items():
+        address = address.replace(key, value) if key in address else address
+        
+    return ' '.join(list(filter(lambda item: item.strip(), address.split())))
+
 def is_empty_or_spaces(s):
     return s.replace(' ', '') == ''
 
@@ -111,16 +151,70 @@ def auto_fit_columns(excel_file_path):
                     
     wb.save(excel_file_path)
 
-def highlight_n_check_prediction(excel_file_path):
+def check_city(area, municipality):
+    if area not in municipality and "CITY" in municipality and "QUEZON" not in municipality:
+        return municipality.replace("CITY", "").strip()
+    return municipality
 
-    def compare_address(str1, address, threshold=60):
-        if str1.replace(' ', '').replace('ñ', 'n').lower() in address.replace(' ', '').replace('ñ', 'n').lower():
-            return False
+def compare_address(str1, address, threshold=60):
+    """
+    Compare a string with an address for similarity.
 
-        similarity_ratio = fuzz.token_set_ratio(clean_string(str(str1)).lower(), clean_string(str(address)).lower())
-        return similarity_ratio <= threshold
-    
+    Parameters:
+    str1 (str): First string to compare.
+    address (str): Address string to compare.
+    threshold (int): Minimum similarity threshold (default is 60).
+
+    Returns:
+    bool: True if the strings are similar, False otherwise.
+    """
+    if str1.replace(' ', '').replace('Ñ', 'N') in address.replace(' ', '').replace('ñ', 'n').replace('Ñ', 'N'):
+        return False
+
+    return True
+
+# def clean_province(province):
+#     """
+#     Clean and normalize a province name.
+
+#     Parameters:
+#     province (str): Input province name.
+
+#     Returns:
+#     str: Cleaned and normalized province name.
+#     """
+#     suffix = ["DEL", "NORTE", "SUR", "DE ORO", "OCCIDENTAL", "ORIENTAL", "EASTERN", "NORTHERN", "SOUTHERN", "WESTERN", "NORTH", "SOUTH", "ISLAND"]
+        
+#     province_parts = province.split()
+        
+#     cleaned_province_parts = [part.strip() for part in province_parts if part.upper() not in suffix]
+        
+#     cleaned_province = " ".join(cleaned_province_parts)
+        
+#     return cleaned_province
+
+
+def remove_numbers(address):
+    """
+    Remove numbers from an address.
+
+    Parameters:
+    address (str): Input address string.
+
+    Returns:
+    str: Address string with numbers removed.
+    """
+    return clean_address(re.sub(r"\d+", "", address).upper())
+
+def highlight_n_check_prediction(excel_file_path):    
+    """
+    Highlight and check prediction accuracy for addresses in the Excel file.
+
+    Parameters:
+    excel_file_path (str): Path to the Excel file.
+    """
     df = pd.read_excel(excel_file_path)
+
     area_index = df.columns.get_loc('AREA') + 1
     municipality_index = df.columns.get_loc('MUNICIPALITY') + 1
     final_area_index = df.columns.get_loc('FINAL AREA') + 1
@@ -129,28 +223,38 @@ def highlight_n_check_prediction(excel_file_path):
     book = load_workbook(excel_file_path)
     sheet = book.active
 
+    pattern_fill = PatternFill(start_color="ffa500", end_color="ffa500", fill_type="solid")
+
     for row_index, row in df.iterrows():
-        address = str(row['ADDRESS']).lower()
+        address = remove_numbers(str(row['ADDRESS']))
 
         cell1 = sheet.cell(row=row_index + 2, column=area_index)
         cell2 = sheet.cell(row=row_index + 2, column=municipality_index)
         cell3 = sheet.cell(row=row_index + 2, column=final_area_index)
         cell4 = sheet.cell(row=row_index + 2, column=autofield_date_index)
 
-        if not address.strip():
-            cell1.value = cell2.value = ''
-        else:
-            cell3.value = cell4.value = ''
+        cell3.value = cell4.value = ''
 
-            area = str(row["AREA"]).lower()
-            municipality = str(row["MUNICIPALITY"]).lower()
+        area = str(row["AREA"])
+        municipality = str(row["MUNICIPALITY"])
 
-            if compare_address(area, address) and compare_address(municipality, address):
-                cell1.fill = cell2.fill = PatternFill(start_color="ffa500", end_color="ffa500", fill_type="solid")
-            elif compare_address(area, address):
-                cell1.fill = PatternFill(start_color="fffa00", end_color="fffa00", fill_type="solid")
-            elif compare_address(municipality, address):
-                cell2.fill = PatternFill(start_color="fffa00", end_color="fffa00", fill_type="solid")
+        if '**' in area and '**' in municipality:
+            cell1.font = cell2.font = Font(bold=True)
+            cell1.value = area = area.replace('**', '')
+            cell2.value = municipality = municipality.replace('**', '')
+
+        if (compare_address(check_city(area, municipality), address)):
+            cell1.fill = cell2.fill = PatternFill(start_color="ffa200", end_color="ffa200", fill_type="solid")
+
+            if not address or len(address) <= 25:
+                cell1.value = cell2.value = ''
+                cell1.fill = cell2.fill = PatternFill(start_color="ff4400", end_color="ff4400", fill_type="solid")
+
+        # elif compare_address(clean_province(area), address):
+        #     cell1.fill = PatternFill(start_color="fffa00", end_color="fffa00", fill_type="solid")
+        elif compare_address(check_city(area, municipality), address):
+           cell2.fill = PatternFill(start_color="fffa00", end_color="fffa00", fill_type="solid")
+            
 
     book.save(excel_file_path)
 
